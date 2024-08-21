bbb075f175f6da33a5f5978404275e6bf882b46c
shanzantrik <shantanu@zantrik.in>
2024-08-22 03:26:59 +0530

    removed eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASEPATH
+  basePath: process.env.BASEPATH,
+eslint: {
+    // Disable the ESLint checks during production build
+    ignoreDuringBuilds: true,
+  }
 }
+
 
 export default nextConfig
